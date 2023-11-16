d7629e6c563d4958e6ec784eb11fc1fabb192b48
Merveille94 <emanueleguehi@gmail.com>
2023-11-16 02:15:48 +0000

    update

--- E-commerce App/scripts.js
+++ E-commerce App/scripts.js
@@ -15,5 +15,10 @@ if (close) {
 }
 
 // Single product page 
+var MainImg = document.getElementById('MainImg');
+var smalling = document.getElementsByClassName('small-img');
 
-var MainImg = document.getElementById('MainImg')
+smalling[0].onclick = function(){ MainImg.src = smalling[0].src; }
+smalling[1].onclick = function(){ MainImg.src = smalling[1].src; }
+smalling[2].onclick = function(){ MainImg.src = smalling[2].src; }
+smalling[3].onclick = function(){ MainImg.src = smalling[3].src; }
